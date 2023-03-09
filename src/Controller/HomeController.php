104bb46e1d0e56e0e441d6a429a6b16456928fb4
Mary-Clb <CSI@campus.local>
2023-03-09 16:24:21 +0100

    fix search

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,28 +11,33 @@ use App\Repository\DeveloperRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    #[Route('/{search}', name: 'app_home_search')]
-    #[Route('/{search}/{page}', name: 'app_home_search_pages')]
-    public function index(?string $search, ?int $page, Request $request, DeveloperRepository $repoDev): Response
+    // #[Route('/search/{search}', name: 'app_home_search')]
+    // #[Route('/search/{search}/{page}', name: 'app_home_search_pages')]
+    public function index(/*?string $search, ?int $page, */Request $request, DeveloperRepository $repoDev): Response
     {   
         $user_role = $this->getUser()->getRoles();
         $role = "";
         $searchResult = [];
 
+        $search = $request->request->get("search");
+        if($search){
+            $searchResult = $repoDev->findByString($search/*, $page, 10*/);
+        }
+
         if (in_array('ROLE_DEV', $user_role)) {
             $role = "dev";
+            return $this->render("home/dev.html.twig", [
+                'searchResult' => $searchResult
+            ]);
         }
         if (in_array('ROLE_COMPANY', $user_role)) {
             $role = "company";
+            return $this->render("home/company.html.twig", [
+                'searchResult' => $searchResult
+            ]);
         }
 
-        $search = $request->request->get("search");
-        if($search){
-            $searchResult = $repoDev->findByString($search, $page, 10);
-            dd($searchResult);
-        }
-
-        return $this->render("home/$role.html.twig", [
+        return $this->render("base.html.twig", [
             'searchResult' => $searchResult
         ]);
     }
