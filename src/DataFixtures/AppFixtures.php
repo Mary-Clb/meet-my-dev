d66ed386007027465eee87510f3f5170bb49f815
NathDLR <86125084+NathDLR@users.noreply.github.com>
2023-06-01 00:07:42 +0200

    Fixtures, searchbar disabled on home

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,11 +2,14 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Company;
+use App\Entity\Activity;
 use App\Entity\Developer;
 use App\Entity\Speciality;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -23,12 +26,20 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr_FR');
 
         $specialities = ['PHP', 'Symfony', 'Javascript', 'React.js', 'Vue.js', 'Bootstrap', 'C#', 'C++', "Python"];
+        $activities = ["Industrie","Marketing","Développement web","Vente","Accompagnement","Santé"];
 
         foreach ($specialities as $i => $spe) {
             $speciality = new Speciality();
             $speciality->setLabel($spe);
             $manager->persist($speciality);
             $specialities[$i] = $speciality;
+        }
+        
+        foreach ($activities as $i => $act) {
+            $activity = new Activity();
+            $activity->setLabel($act);
+            $manager->persist($activity);
+            $activities[$i] = $activity;
         }
 
         $developers = Array();
@@ -37,17 +48,41 @@ class AppFixtures extends Fixture
             $developers[$i]->setname($faker->lastName);
             $developers[$i]->setfirstname($faker->firstName);
             $developers[$i]->setExperience($faker->numberBetween(0, 40));
-            $developers[$i]->setPresentation('jkljkljhjkhkgbjh jhhbjgk kbhjg bkjbkjbl16584110.0');
+            $developers[$i]->setPresentation($faker->realText());
             $developers[$i]->addSpeciality($specialities[rand(0, 8)]);
-            $developers[$i]->setUsername('Toto' . $i);
+            $developers[$i]->setUsername($faker->userName);
             $developers[$i]->setMail($faker->email);
             $developers[$i]->setTelephone($faker->phoneNumber);
             $password = $this->hasher->hashPassword($developers[$i], 'mdp');
             $developers[$i]->setPassword($password);
 
             $manager->persist($developers[$i]);
-
         }
+
+        for ($i = 0; $i < 6; $i++) {
+            $companies[$i] = new Company();
+            $companies[$i]->setname($faker->company);
+            $companies[$i]->setemployees($faker->numberBetween(0, 40));
+            $companies[$i]->setSiret($faker->numberBetween(100000000, 999999999));
+            $companies[$i]->setPresentation($faker->realText());
+            $companies[$i]->setPublique($faker->boolean());
+            $companies[$i]->addActivity($activities[rand(0, 5)]);
+            $companies[$i]->setUsername($faker->userName);
+            $companies[$i]->setMail($faker->email);
+            $companies[$i]->setTelephone($faker->phoneNumber);
+            $password = $this->hasher->hashPassword($companies[$i], 'mdp');
+            $companies[$i]->setPassword($password);
+
+            $manager->persist($companies[$i]);
+        }
+
+        $admin = new User();
+        $admin->setRoles(['ROLE_ADMIN'])
+        ->setUsername('admin')
+        ->setMail('admin@mail.fr')
+        ->setPassword($this->hasher->hashPassword($admin, 'admin'));
+
+        $manager->persist($admin);
         $manager->flush();
     }
 }
